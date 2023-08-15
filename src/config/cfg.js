77a22d8826109236459077b95184213da422394d
tmalcher <malcher.tm@gmail.com>
2023-08-15 17:38:33 -0400

    fixed bug

--- src/config/cfg.js
+++ src/config/cfg.js
@@ -1,6 +1,6 @@
 const cfg = ()=>{
     return {
-        jwt_secret:"rafael",
+        jwt_secret:"thiago",
         jwt_expires:"1d",
         salt:10,
         db:"mongodb+srv://mbafiap:UFDhaROPAGxiWZjJ@mbafiap.83wxhok.mongodb.net/?retryWrites=true&w=majority"
